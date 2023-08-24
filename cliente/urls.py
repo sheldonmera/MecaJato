607b39de924517a7af84fdfd4aaf91e4af6cdfe0
sheldon mera <eddiemera@hotmail.com>
2023-08-24 19:30:57 -0400

    creation of busca e update cliente on urls.py

--- cliente/urls.py
+++ cliente/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.cliente, name='cliente'),
-    path('atualiza_cliente/', views.atualiza_cliente, name='atualiza_cliente'),
+    path('busca_cliente/', views.busca_cliente, name='busca_cliente'),
+    path('update_cliente/<int:id>', views.update_cliente, name='update_cliente'),
     path('update_carro/<int:id>', views.update_carro, name='update_carro'),
     path('delete_carro/<int:id>', views.delete_carro, name='delete_carro'),
 ]
